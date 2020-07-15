bce40017aba03d6d0e691827cf4681d7fcff9fb0
Augusto Alonso <augustoalonso98@gmail.com>
2020-07-15 14:52:18 -0600

    version 1.0.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import Room from './lib/Room'
-import { VideoQualities, FrameRates, FacingModes } from './lib/RoomBuilder'
-import RoomBuilder from './lib/RoomBuilder'
-import Client from './lib/Client'
+import Room from './src/Room'
+import { VideoQualities, FrameRates, FacingModes } from './src/RoomBuilder'
+import RoomBuilder from './src/RoomBuilder'
+import Client from './src/Client'
 
 export { Room, RoomBuilder, Client, VideoQualities, FrameRates, FacingModes }
