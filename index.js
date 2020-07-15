213181c7414e24c622001a3e4f18da2916157288
Augusto Alonso <augustoalonso98@gmail.com>
2020-07-15 14:47:09 -0600

    add git ignore

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import Room from './Room'
-import { VideoQualities, FrameRates, FacingModes } from './RoomBuilder'
-import RoomBuilder from './RoomBuilder'
-import Client from './Client'
+import Room from './lib/Room'
+import { VideoQualities, FrameRates, FacingModes } from './lib/RoomBuilder'
+import RoomBuilder from './lib/RoomBuilder'
+import Client from './lib/Client'
 
 export { Room, RoomBuilder, Client, VideoQualities, FrameRates, FacingModes }
